0a7be477b6d2e4c927e6db6b8a12055ee0ca2f69
Crawcik <crawcik@gmail.com>
2022-11-16 11:52:18 +0100

    Compability fix for 1.4

--- Source/Interay/Interay.Plugin.cs
+++ Source/Interay/Interay.Plugin.cs
@@ -8,6 +8,7 @@ namespace Interay
 	/// </summary>
 	internal class PluginInfo : GamePlugin
 	{
+		#if FLAX_1_3 || FLAX_1_2 || FLAX_1_1 || FLAX_1_0
 		/// <inheritdoc />
 		public override PluginDescription Description => new PluginDescription
 		{
@@ -22,5 +23,23 @@ namespace Interay
 			IsAlpha = true,
 			IsBeta = false,
 		};
+		#else
+		public PluginInfo() : base()
+		{
+			_description = new PluginDescription
+			{
+				Name = "Interay",
+				Category = "Network",
+				Author = "Crawcik",
+				AuthorUrl = "https://github.com/Crawcik",
+				HomepageUrl = "https://github.com/Crawcik/Interay",
+				RepositoryUrl = "https://github.com/Crawcik/Interay",
+				Description = "Flexible networking plugin for many multiplayer games.",
+				Version = new Version(1, 0),
+				IsAlpha = true,
+				IsBeta = false,
+			};
+		}
+		#endif
 	}
 }
